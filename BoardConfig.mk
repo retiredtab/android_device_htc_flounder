cdcccc50282e3a7d93d60839bbbb6a34c019a267
Aaron Kling <webgeek1234@gmail.com>
2019-11-16 02:11:57 +0000

    flounder: Migrate zygote whitelist to newer convention
    Change-Id: Ia0faeb128378dd760a0af7fae5d8059044d4d961

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,6 +125,9 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 
 MALLOC_SVELTE := true
 
+# Zygote Whitelist
+ZYGOTE_WHITELIST_PATH_EXTRA := \"/dev/nvhost-ctrl\",
+
 # Forcebly use the non-open-source parts
 include vendor/htc/flounder-common/BoardConfigVendor.mk
 ifeq ($(TARGET_PRODUCT),lineage_flounder_lte)
