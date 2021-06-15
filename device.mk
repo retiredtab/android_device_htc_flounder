3fa4926d2c0891935618585926c9ff44f7998d19
Andrea <linckandrea01@gmail.com>
2021-06-15 19:51:06 +0200

    flounder: libshims: kang zygote_whitelist from yellowstone
     * as https://github.com/LineageOS/android_device_google_yellowstone/commit/1067d69c989197a9f03a38b58c332d3007a69d80#diff-1d0afc0e5abf6a5337d23d1241cf817aca4a613ad79bd11a08c4d237526ac95f

--- device.mk
+++ device.mk
@@ -204,6 +204,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Libshims
+PRODUCT_PACKAGES += \
+    libshim_zw
+
 # Light HAL
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.flounder
