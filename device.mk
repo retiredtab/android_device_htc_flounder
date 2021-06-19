a40db7a1a9c08b072bc955bdb8dc67c7b577aa95
Andrea <linckandrea01@gmail.com>
2021-06-19 16:42:39 +0200

    flounder: build usb 1.0 basic HAL

--- device.mk
+++ device.mk
@@ -238,7 +238,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.flounder
+    android.hardware.usb@1.0-service.basic
 
 # Vibrator
 PRODUCT_PACKAGES += \
